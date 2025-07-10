6950bd0418de493140bfb34ec1195e0bf1475777
Sellami Mohamed <sellamimohamed71@gmail.com>
2025-07-10 20:56:01 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,1 @@
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("🌀 Commercium site loaded.");
-  // Future interactions can be added here.
-});
+// Future interactivity can go here
